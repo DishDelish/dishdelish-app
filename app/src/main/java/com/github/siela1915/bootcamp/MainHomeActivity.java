defea4504531558c1eea7ccbb77dd0709ce1795b
bilalkakar01 <73235289+bilalkakar01@users.noreply.github.com>
2023-03-13 10:36:39 +0100

    dependencies added to gradle.build

---
+++ app/src/main/java/com/github/siela1915/bootcamp/MainHomeActivity.java
@@ -0,0 +1,14 @@
+package com.github.siela1915.bootcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home2);
+    }
+}
