f9bb5df4c56249247c1975c11b815c3f2137c425
bilalkakar01 <73235289+bilalkakar01@users.noreply.github.com>
2023-03-13 11:23:04 +0100

    set the HomePageFragment as default view in MainHomeActivity.java file

--- app/src/main/java/com/github/siela1915/bootcamp/MainHomeActivity.java
+++ app/src/main/java/com/github/siela1915/bootcamp/MainHomeActivity.java
@@ -1,15 +1,28 @@
 package com.github.siela1915.bootcamp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainHomeActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home2);
 
+        if(savedInstanceState== null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragContainer,HomePageFragment.class,null)
+                    .commit();
+        }
     }
 }
