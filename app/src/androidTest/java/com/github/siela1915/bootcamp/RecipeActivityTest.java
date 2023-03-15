7dc902f55c29ef0f7ca3c40b8025aa3e8472d2af
eylulipci <eylulipci00@gmail.com>
2023-03-15 14:35:25 +0100

    add more tests

--- app/src/androidTest/java/com/github/siela1915/bootcamp/RecipeActivityTest.java
+++ app/src/androidTest/java/com/github/siela1915/bootcamp/RecipeActivityTest.java
@@ -1,51 +1,179 @@
 package com.github.siela1915.bootcamp;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 
-import org.junit.Rule;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
+import com.github.siela1915.bootcamp.Recipes.ExampleRecipes;
+import com.github.siela1915.bootcamp.Recipes.Recipe;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 
 public class RecipeActivityTest {
 
-    @Rule
-    public ActivityScenarioRule<RecipeActivity> testRule = new ActivityScenarioRule<>(RecipeActivity.class);
+    Recipe omelette = ExampleRecipes.recipes.get(0);
+    Intent i = RecipeConverter.convertToIntent(omelette, ApplicationProvider.getApplicationContext());
 
     @Test
-    public void isRecipePictureOnDisplay(){
+    public void isRecipePictureOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
         onView(withId(R.id.recipePicture)).check(matches(isDisplayed()));
+        scenario.close();
     }
 
     @Test
-    public void isRecipeTitleOnDisplay(){
-        onView(withId(R.id.recipeNameText)).check(matches(isDisplayed()));
+    public void isCorrectRecipeNameOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.recipeNameText)).check(matches(withText(omelette.recipeName)));
+        scenario.close();
+
     }
 
     @Test
-    public void isRatingOnDisplay(){
-        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
+    public void isCorrectUserNameOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.userNameText)).check(matches(withText(omelette.userName)));
+        scenario.close();
     }
 
     @Test
-    public void isUserNameOnDisplay(){
-        onView(withId(R.id.userNameText)).check(matches(isDisplayed()));
-    }
-
-    
-
-    @Test
-    public void isFavoritesButtonOnDisplay(){
-        onView(withId(R.id.favoriteImage)).check(matches(isDisplayed()));
+    public void isCorrectCookTimeDisplayed() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.cookTimeNbMins)).check(matches(withText(String.valueOf(omelette.cookTime))));
+        scenario.close();
     }
 
     @Test
-    public void areInfoBoxesOnDisplay(){
-        onView(withId(R.id.infoBoxes)).check(matches(isDisplayed()));
+    public void isCorrectServingsOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.nbServings)).check(matches(withText(String.valueOf(omelette.servings))));
+        scenario.close();
     }
 
+    @Test
+    public void isCorrectIngredientsListOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.ingredientsList)).check(matches(withText(String.join("\n", RecipeConverter.convertIngredientList(omelette.ingredientList)))));
+        scenario.close();
+    }
+
+    @Test
+    public void isCorrectUtensilsListOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.utensilsList)).check(matches(withText(String.join(", ", omelette.utensils.getUtensils()))));
+        scenario.close();
+    }
+
+    @Test
+    public void areCorrectsStepsDisplayed() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.stepsText)).check(matches(withText(String.join("\n\n", omelette.steps))));
+        scenario.close();
+    }
+
+    @Test
+    public void areCorrectCommentsOnDisplay() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.commentsText)).check(matches(withText(String.join("\n", omelette.comments))));
+        scenario.close();
+    }
+
+    @Test
+    public void isRatingCorrect() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.ratingBar)).check(matches(withRating((float) omelette.rating)));
+        scenario.close();
+
+    }
+
+    @Test
+    public void isRatingActivityStarted() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        Intents.init();
+        onView(withId(R.id.rateButton))
+                .perform(scrollTo(), click());
+
+        intended(allOf(hasComponent(RatingActivity.class.getName())));
+
+        Intents.release();
+        scenario.close();
+    }
+
+    @Test
+    public void isCorrectRecipePictureDisplayed() {
+        ActivityScenario scenario = ActivityScenario.launch(i);
+        onView(withId(R.id.recipePicture))
+                .check(matches(withDrawable(omelette.image)));
+        scenario.close();
+    }
+
+    public static Matcher<View> withRating(final float rating) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(org.hamcrest.Description description) {
+                description.appendText("with rating: " + rating);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof RatingBar)) {
+                    return false;
+                }
+                RatingBar ratingBar = (RatingBar) view;
+                return ratingBar.getRating() == rating;
+            }
+        };
+    }
+
+    public static Matcher<View> withDrawable(final int expectedResourceId) {
+        return new BoundedMatcher<View, ImageView>(ImageView.class) {
+            private Bitmap expectedBitmap;
+            ;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with drawable resource id: ");
+                description.appendValue(expectedResourceId);
+            }
+
+            @Override
+            public boolean matchesSafely(ImageView imageView) {
+                Resources resources = imageView.getContext().getResources();
+                expectedBitmap = BitmapFactory.decodeResource(resources, expectedResourceId);
+                Drawable actualDrawable = imageView.getDrawable();
+                if (actualDrawable instanceof BitmapDrawable) {
+                    Bitmap actualBitmap = ((BitmapDrawable) actualDrawable).getBitmap();
+                    return expectedBitmap.sameAs(actualBitmap);
+                } else {
+                    return false;
+                }
+            }
+        };
+    }
 
 }
--- app/src/main/java/com/github/siela1915/bootcamp/RecipeActivity.java
+++ app/src/main/java/com/github/siela1915/bootcamp/RecipeActivity.java
@@ -3,8 +3,15 @@ package com.github.siela1915.bootcamp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -12,6 +19,10 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        Intent i = getIntent();
+
+        setPageContents(i);
 
         Button rateButton = (Button) findViewById(R.id.rateButton);
         rateButton.setOnClickListener(v -> {
@@ -22,6 +33,44 @@ public class RecipeActivity extends AppCompatActivity {
             v.getContext().startActivity(ratingIntent);
         });
 
+
+    }
+
+    private void setPageContents(Intent i){
+
+        ImageView recipePicture =(ImageView) findViewById(R.id.recipePicture);
+        TextView recipeNameText =(TextView) findViewById(R.id.recipeNameText);
+        ImageView userAvatar=(ImageView) findViewById(R.id.userAvatar);
+        TextView userNameText=(TextView) findViewById(R.id.userNameText);
+        TextView prepTime=(TextView) findViewById(R.id.prepTimeNbMins);
+        TextView kcalCount=(TextView) findViewById(R.id.kcalCount);
+        TextView cookTime=(TextView) findViewById(R.id.cookTimeNbMins);
+        TextView nbServings=(TextView) findViewById(R.id.nbServings);
+        TextView ingredientsList=(TextView) findViewById(R.id.ingredientsList);
+        TextView utensilsList=(TextView) findViewById(R.id.utensilsList);
+        TextView stepsText=(TextView) findViewById(R.id.stepsText);
+        TextView commentsText=(TextView) findViewById(R.id.commentsText);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        Bitmap recipeImage = BitmapFactory.decodeResource(this.getResources(), i.getIntExtra("image",0));
+        recipePicture.setImageBitmap(recipeImage);
+
+        Bitmap avatar = BitmapFactory.decodeResource(this.getResources(), i.getIntExtra("profilePicture",0));
+        userAvatar.setImageBitmap(avatar);
+
+        recipeNameText.setText(i.getStringExtra("recipeName"));
+        userNameText.setText(i.getStringExtra("userName"));
+
+        ratingBar.setRating((float) i.getDoubleExtra("rating", 0.0));
+        prepTime.setText(String.valueOf(i.getIntExtra("prepTime",0)));
+
+        cookTime.setText(String.valueOf(i.getIntExtra("cookTime",0)));
+        nbServings.setText(String.valueOf(i.getIntExtra("servings",0)));
+        utensilsList.setText(String.join(", ", i.getStringArrayExtra("utensils")));
+
+        ingredientsList.setText(String.join("\n", i.getStringArrayListExtra("ingredients")));
+        stepsText.setText(String.join("\n\n", i.getStringArrayExtra("steps")));
+        commentsText.setText(String.join("\n", i.getStringArrayExtra("comments")));
 
     }
 }
