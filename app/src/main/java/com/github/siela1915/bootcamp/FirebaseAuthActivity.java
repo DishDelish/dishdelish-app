fe9eabd9bd401b625fbc8a0dfff823827809edb5
Elias Boschung <siela1915@gmail.com>
2023-03-16 01:00:32 +0100

    Add static promptLogin function, which asks the user if he wants to login now or later

--- app/src/main/java/com/github/siela1915/bootcamp/FirebaseAuthActivity.java
+++ app/src/main/java/com/github/siela1915/bootcamp/FirebaseAuthActivity.java
@@ -1,12 +1,14 @@
 package com.github.siela1915.bootcamp;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -174,4 +176,28 @@ public class FirebaseAuthActivity extends AppCompatActivity {
         return Tasks.forCanceled();
     }
 
+    public static void promptLogin(Context context, Activity activity, Intent postLoginIntent) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            return;
+        }
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        @SuppressLint("InflateParams") View popupView = inflater.inflate(R.layout.prompt_login_popup, null);
+
+        Button loginButton = popupView.findViewById(R.id.loginPromptLoginButton);
+        Button cancelButton = popupView.findViewById(R.id.loginPromptLaterButton);
+
+        int width_height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width_height, width_height, focusable);
+
+        loginButton.setOnClickListener(view -> {
+            activity.startActivity(createIntent(context, AUTH_ACTION.LOGIN, postLoginIntent));
+            popupWindow.dismiss();
+        });
+        cancelButton.setOnClickListener(view -> popupWindow.dismiss());
+
+        popupWindow.showAtLocation(activity.findViewById(android.R.id.content).getRootView(), Gravity.CENTER, 0, 0);
+    }
 }
