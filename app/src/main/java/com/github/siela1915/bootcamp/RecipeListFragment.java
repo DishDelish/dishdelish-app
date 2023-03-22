e0d72edb08618d944611e730226df95e07515824
Elias Boschung <siela1915@gmail.com>
2023-03-22 18:47:50 +0100

    Add a first version of a Recipe List Fragment

---
+++ app/src/main/java/com/github/siela1915/bootcamp/RecipeListFragment.java
@@ -0,0 +1,68 @@
+package com.github.siela1915.bootcamp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.siela1915.bootcamp.Recipes.Recipe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class RecipeListFragment extends Fragment {
+
+    private static final String ARG_RECIPE_LIST = "recipe-list";
+    private List<Recipe> mRecipeList = Collections.emptyList();
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public RecipeListFragment() {
+    }
+
+    public static RecipeListFragment newInstance(List<Recipe> recipeList) {
+        RecipeListFragment fragment = new RecipeListFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARG_RECIPE_LIST, new ArrayList<>(recipeList));
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mRecipeList = getArguments().getParcelableArrayList(ARG_RECIPE_LIST);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new RecipeItemRecyclerViewAdapter(mRecipeList));
+            recyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
+        }
+        return view;
+    }
+}
