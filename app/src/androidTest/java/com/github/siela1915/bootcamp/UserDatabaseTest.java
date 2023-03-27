3e060c20e3c78c02ab3d0c0dba8f05a50de3cd1e
Elias Boschung <siela1915@gmail.com>
2023-03-27 23:53:38 +0200

    Add tests for user database class

---
+++ app/src/androidTest/java/com/github/siela1915/bootcamp/UserDatabaseTest.java
@@ -0,0 +1,235 @@
+package com.github.siela1915.bootcamp;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThrows;
+
+import android.net.Uri;
+import android.security.keystore.UserNotAuthenticatedException;
+
+import com.github.siela1915.bootcamp.firebase.User;
+import com.github.siela1915.bootcamp.firebase.UserDatabase;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
+public class UserDatabaseTest {
+    private UserDatabase userDb;
+
+    @Before
+    public void prepareEmulator() {
+        FirebaseAuth.getInstance().useEmulator("10.0.2.2", 9099);
+        FirebaseDatabase.getInstance().useEmulator("10.0.2.2", 9000);
+
+        if (userDb == null) {
+            userDb = new UserDatabase();
+        }
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuthActivityTest.logoutSync();
+        }
+    }
+
+    @Test
+    public void getInexistentUserReturnsNull() {
+        try {
+            User inexistent = Tasks.await(userDb.getUser("inexistent"));
+            assertThat(inexistent, is(nullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void setThenGetReturnsNewUser() {
+        User testUser = new User();
+        testUser.setId("setThenGetReturnsNewUser");
+        testUser.setEmail("setThenGetReturnsNewUser@example.com");
+        testUser.setDisplayName("setThenGet ReturnsNewUser");
+        testUser.setPhotoUrl("https://setThenGet.ReturnsNewUser");
+
+        try {
+            User dbUser = Tasks.await(userDb.updateUser(testUser).continueWithTask(task -> userDb.getUser("setThenGetReturnsNewUser")));
+            assertThat(dbUser.getDisplayName(), is(testUser.getDisplayName()));
+            assertThat(dbUser.getEmail(), is(testUser.getEmail()));
+            assertThat(dbUser.getPhotoUrl(), is(testUser.getPhotoUrl()));
+            assertThat(dbUser.getId(), is(testUser.getId()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void updateProfileWithFirebaseUser() {
+        FirebaseAuthActivityTest.loginSync("preUpdateProfile@example.com");
+        User testUser = new User();
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        testUser.setId(userId);
+        testUser.setEmail("preUpdateProfile@example.com");
+        testUser.setDisplayName("preUpdate Profile");
+        testUser.setPhotoUrl("https://preUpdate.Profile");
+
+        UserProfileChangeRequest changeUser = new UserProfileChangeRequest.Builder()
+                .setDisplayName("postUpdate Profile")
+                .setPhotoUri(null)
+                .build();
+        try {
+            User dbUser = Tasks.await(userDb.updateUser(testUser)
+                    .continueWithTask(task -> FirebaseAuthActivity.update(changeUser))
+                    .continueWithTask(task -> Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).updateEmail("postUpdateProfile@example.com"))
+                    .continueWithTask(task -> userDb.updateProfile(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser())))
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser.getEmail(), is("postUpdateProfile@example.com"));
+            assertThat(dbUser.getPhotoUrl(), is(nullValue()));
+            assertThat(dbUser.getDisplayName(), is("postUpdate Profile"));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseAuthActivityTest.logoutSync();
+    }
+
+    @Test
+    public void addDeviceTokenToExistingUser() {
+        FirebaseAuthActivityTest.loginSync("addDeviceTokenToExisting@example.com");
+        User testUser = new User();
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        testUser.setId(userId);
+        testUser.setEmail("addDeviceTokenToExisting@example.com");
+        testUser.setDisplayName("addDeviceToken ToExisting");
+        testUser.setPhotoUrl("https://addDeviceToken.ToExisting");
+
+        userDb.updateUser(testUser);
+
+        try {
+            User dbUser = Tasks.await(FirebaseAuthActivity.update(new UserProfileChangeRequest.Builder()
+                    .setPhotoUri(new Uri.Builder().appendPath(testUser.getPhotoUrl()).build()).build())
+                    .continueWithTask(task -> userDb.addDeviceToken())
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser.getTokens().size(), is(1));
+            String token = Tasks.await(FirebaseMessaging.getInstance().getToken());
+            assertThat(dbUser.getTokens().get(token), is(notNullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseAuthActivityTest.logoutSync();
+    }
+
+    @Test
+    public void addDeviceTokenToInexistentUser() {
+        FirebaseAuthActivityTest.loginSync("addDeviceTokenToInexistent@example.com");
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        try {
+            User dbUser = Tasks.await(userDb.addDeviceToken()
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser.getTokens().size(), is(1));
+            String token = Tasks.await(FirebaseMessaging.getInstance().getToken());
+            assertThat(dbUser.getTokens().get(token), is(notNullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseAuthActivityTest.logoutSync();
+    }
+
+    @Test
+    public void removeExistentDeviceToken() {
+        FirebaseAuthActivityTest.loginSync("removeExistentDeviceToken@example.com");
+        User testUser = new User();
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        testUser.setId(userId);
+        testUser.setEmail("removeExistentDeviceToken@example.com");
+        testUser.setDisplayName("removeExistent DeviceToken");
+        testUser.setPhotoUrl("https://removeExistent.DeviceToken");
+
+        userDb.updateUser(testUser);
+
+        try {
+            User dbUser = Tasks.await(userDb.addDeviceToken()
+                    .continueWithTask(task -> userDb.removeDeviceToken())
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser.getTokens().size(), is(0));
+            String token = Tasks.await(FirebaseMessaging.getInstance().getToken());
+            assertThat(dbUser.getTokens().get(token), is(nullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseAuthActivityTest.logoutSync();
+    }
+
+    @Test
+    public void removeInexistentDeviceToken() {
+        FirebaseAuthActivityTest.loginSync("removeInexistentDeviceToken@example.com");
+        User testUser = new User();
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        testUser.setId(userId);
+        testUser.setEmail("removeInexistentDeviceToken@example.com");
+        testUser.setDisplayName("removeInexistent DeviceToken");
+        testUser.setPhotoUrl("https://removeInexistent.DeviceToken");
+
+        userDb.updateUser(testUser);
+
+        try {
+            User dbUser = Tasks.await(userDb.removeDeviceToken()
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser.getTokens().size(), is(0));
+            String token = Tasks.await(FirebaseMessaging.getInstance().getToken());
+            assertThat(dbUser.getTokens().get(token), is(nullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseAuthActivityTest.logoutSync();
+    }
+
+    @Test
+    public void removeDeviceTokenFromInexistentUser() {
+        FirebaseAuthActivityTest.loginSync("removeDeviceTokenFromInexistentUser@example.com");
+        String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+
+        try {
+            User dbUser = Tasks.await(userDb.removeDeviceToken()
+                    .continueWithTask(task -> userDb.getUser(userId)));
+            assertThat(dbUser, is(nullValue()));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void addOrRemoveDeviceTokenWhileLoggedOut() {
+        assertThrows(UserNotAuthenticatedException.class, () -> {
+            try {
+                Task<Void> task = userDb.removeDeviceToken();
+                Tasks.await(task);
+            } catch (ExecutionException exception) {
+                if (exception.getCause() != null) {
+                    throw exception.getCause();
+                }
+            }
+        });
+        assertThrows(UserNotAuthenticatedException.class, () -> {
+            try {
+                Task<Void> task = userDb.addDeviceToken();
+                Tasks.await(task);
+            } catch (ExecutionException exception) {
+                if (exception.getCause() != null) {
+                    throw exception.getCause();
+                }
+            }
+        });
+    }
+}
