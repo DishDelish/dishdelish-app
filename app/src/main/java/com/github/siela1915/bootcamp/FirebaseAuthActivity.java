eece7f621917fe9dc123ee168fd99036134698db
Elias Boschung <siela1915@gmail.com>
2023-03-27 17:24:13 +0200

    Update the user in the firebase database when logging in/out

--- app/src/main/java/com/github/siela1915/bootcamp/FirebaseAuthActivity.java
+++ app/src/main/java/com/github/siela1915/bootcamp/FirebaseAuthActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.security.keystore.UserNotAuthenticatedException;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,7 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.github.siela1915.bootcamp.firebase.UserDatabase;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
@@ -92,14 +94,20 @@ public class FirebaseAuthActivity extends AppCompatActivity {
             // Successfully signed in
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             if (user != null) {
-                // Launch supplied intent with the user info
-                finish();
-                if (postAuthIntent == null) {
-                    this.onBackPressed();
-                } else {
-                    startActivity(postAuthIntent);
-                }
+                UserDatabase userDb = new UserDatabase();
+                userDb.addDeviceToken().addOnCompleteListener(voidTask -> {
+                    // Launch supplied intent with the user info
+                    finish();
+                    if (postAuthIntent == null) {
+                        this.onBackPressed();
+                    } else {
+                        startActivity(postAuthIntent);
+                    }
+                });
             }
+        }
+        if (response == null) {
+            this.onBackPressed();
         }
         if (response != null && response.getError() != null) { // If response is null, user canceled with back button, so no popup
             // inflate the layout of the popup window
@@ -129,6 +137,8 @@ public class FirebaseAuthActivity extends AppCompatActivity {
     public void signIn() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
+            UserDatabase userDb = new UserDatabase();
+            userDb.addDeviceToken();
             finish();
             if (postAuthIntent == null) {
                 this.onBackPressed();
@@ -150,8 +160,9 @@ public class FirebaseAuthActivity extends AppCompatActivity {
     }
 
     public void signOut() {
-        AuthUI.getInstance()
-                .signOut(this)
+        UserDatabase userDb = new UserDatabase();
+        userDb.removeDeviceToken().continueWithTask(voidTask -> AuthUI.getInstance()
+                .signOut(this))
                 .addOnCompleteListener(task -> {
                     finish();
                     if (postAuthIntent == null) {
@@ -179,9 +190,9 @@ public class FirebaseAuthActivity extends AppCompatActivity {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
             FirebaseUser user = auth.getCurrentUser();
-            return user.updateProfile(changeRequest);
+            return user.updateProfile(changeRequest).continueWithTask(task -> new UserDatabase().updateProfile(user));
         }
-        return Tasks.forCanceled();
+        return Tasks.forException(new UserNotAuthenticatedException("User needs to be authenticated to add device token"));
     }
 
     public static void promptLogin(Context context, Activity activity, Intent postLoginIntent) {
