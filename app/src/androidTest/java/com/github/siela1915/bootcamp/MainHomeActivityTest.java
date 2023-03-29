8751e6d38245f33e2f6f3c19b2b35f9409ac9e8c
bilalkakar01 <73235289+bilalkakar01@users.noreply.github.com>
2023-03-29 13:56:05 +0200

    added some tests

--- app/src/androidTest/java/com/github/siela1915/bootcamp/MainHomeActivityTest.java
+++ app/src/androidTest/java/com/github/siela1915/bootcamp/MainHomeActivityTest.java
@@ -1,15 +1,24 @@
 package com.github.siela1915.bootcamp;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.allOf;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.github.siela1915.bootcamp.Labelling.CuisineType;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -68,5 +77,55 @@ public class MainHomeActivityTest {
         onView(withId(R.id.navView)).perform(NavigationViewActions.navigateTo(R.id.menuItem_favorites));
         onView(withId(R.id.homeFragment)).check(matches(isDisplayed()));
         
+    }
+    @Test
+    public void testOnChosingFilter(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.navView))
+                .perform(NavigationViewActions.navigateTo(R.id.menuItem_filter));
+        onView(withId(R.id.homeFragment)).check(matches(isDisplayed()));
+        onView(withId(R.id.filterLayout)).check(matches(isDisplayed()));
+        onView(withId(R.id.cuisineBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.notIncludIngBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.dietBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.timingBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.filterBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.cuisineLabel)).check(matches (isDisplayed()));
+        onView(withId(R.id.prepTimeLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.dietLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.notIncludLabel)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testingCuisineTypeBtn(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.navView))
+                .perform(NavigationViewActions.navigateTo(R.id.menuItem_filter));
+        onView(withId(R.id.cuisineBtn)).perform(click());
+        onView(withText("Chose your preferred cuisine")).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isClickable()));
+        onView(withText("Ok")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Ok")).inRoot(isDialog()).perform(click());
+        onView(withId(R.id.filterLayout)).check(matches(isDisplayed()));
+        onView(withId(R.id.cuisineBtn)).perform(click());
+        onView(withText("Cancel")).inRoot(isDialog()).check(matches(isClickable()));
+        onView(withText("Cancel")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(withText("Cancel")).inRoot(isDialog()).perform(click());
+        onView(withId(R.id.filterLayout)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testingDietTypeBtn(){
+
+    }
+    @Test
+    public void testingAllergyBtn(){
+
+    }
+    @Test
+    public void testingPrepTimeBtn(){
+
+    }
+    @Test
+    public void testingFilterBtn(){
+
     }
 }
