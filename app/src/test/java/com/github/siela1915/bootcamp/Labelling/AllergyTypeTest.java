29470d35ebabe90d40f7638678acc1fc8056226f
bilalkakar01 <73235289+bilalkakar01@users.noreply.github.com>
2023-03-30 14:31:49 +0200

    added some more tests

--- app/src/test/java/com/github/siela1915/bootcamp/Labelling/AllergyTypeTest.java
+++ app/src/test/java/com/github/siela1915/bootcamp/Labelling/AllergyTypeTest.java
@@ -11,8 +11,14 @@ import org.junit.Test;
 public class AllergyTypeTest extends TestCase {
 
     @Test
-    public void test1(){
+    public void getAllReturnsAllAllergyEnums(){
         String[] at= {"Eggs","Fish","Crustaceans","Tree nuts","Peanuts","Wheat","Soybeans","Sesame","Milk"};
         assertThat(at, is(AllergyType.getAll()));
+    }
+
+    @Test
+    public void fromStringReturnTheCorrectEnumFromGivenString(){
+        AllergyType allergyType= AllergyType.EGGS;
+        assertEquals(allergyType,AllergyType.fromString("Eggs"));
     }
 }
