b1a48ad51b4756752a8d8fbf96dbd4098a2aa45d
bilalkakar01 <73235289+bilalkakar01@users.noreply.github.com>
2023-05-16 15:26:27 +0200

    added some tests

--- app/src/main/java/com/github/siela1915/bootcamp/FragmentIngredientCheck.java
+++ app/src/main/java/com/github/siela1915/bootcamp/FragmentIngredientCheck.java
@@ -37,7 +37,7 @@ public class FragmentIngredientCheck extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_INGREDIENT_LIST = "ingredients";
+    public static final String ARG_INGREDIENT_LIST = "ingredients";
     //private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
